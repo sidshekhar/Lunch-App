2f803ada069bc53c96324fad25882d4b7302f2ea
Sid Shekhar <sidshekhar@users.noreply.github.com>
2017-05-01 18:06:10 +0100

    Comments for clarification on keys

--- js/services/placesExplorerService.js
+++ js/services/placesExplorerService.js
@@ -1,4 +1,5 @@
 var requestParms = {
+    //Sample Client Keys 
     clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
     clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
     version: "20131230"
