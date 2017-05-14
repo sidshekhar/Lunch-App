cc93ab304d5e5f791b5ae2b7f59023641a764d17
Sid Shekhar <sidshekhar@users.noreply.github.com>
2017-05-14 10:48:08 +0100

    update coordinates

--- js/services/location.js
+++ js/services/location.js
@@ -5,7 +5,7 @@ app.factory('myCoordinates', ['$q', function myCoordinates($q) {
 	function getCoordinates(coordinates){
 		var myCoordinates = {};
 		myCoordinates.lat = 51.5074;  //London Latitude	
-		myCoordinates.lng = 0.1278;   //London Longitude
+		myCoordinates.lng = -0.118092	;   //London Longitude
 		deferred.resolve(myCoordinates);
 	}
 
